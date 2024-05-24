9a5b85d64703aff697c0c7a383f6cbcab42bf3e1
cloudwithax <heyimnick14@gmail.com>
2024-05-24 01:17:55 -0400

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,91 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
+'use client';
+import { Select, SelectSection, SelectItem, useDisclosure, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Switch, Spinner, Textarea, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Divider, Button, Avatar } from "@nextui-org/react";
+import { useState } from "react";
+import type { UseDisclosureReturn } from '@nextui-org/use-disclosure';
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+
+
+
 
 export default function Home() {
+
+  const settingsModal = useDisclosure();
+
+  function SettingsModal({ disclosure }: { disclosure: UseDisclosureReturn }) {
+    const { isOpen, onOpen, onOpenChange, onClose } = disclosure;
+
+
+    return (
+      <>
+        <Modal hideCloseButton placement="center" className="dark text-foreground" isOpen={isOpen} onOpenChange={onOpenChange}>
+          <ModalContent>
+            {(onClose: any) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">Settings</ModalHeader>
+                <ModalBody className="mb-4">
+                  <div className="flex flex-row justify-between grow gap-4">
+                    <p>Select</p>
+                    <Select
+                      size="sm"
+                      className="w-32 text-wrap static bottom-0"
+                    >
+                      <SelectSection>
+                        <SelectItem key={1} value="1">1</SelectItem>
+                        <SelectItem key={2} value="2">2</SelectItem>
+                        <SelectItem key={3} value="3">3</SelectItem>
+
+                      </SelectSection>
+                    </Select>
+                  </div>
+
+
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="danger" variant="light" onClick={onClose}>
+                    Close
+                  </Button>
+                  <Button color="primary">
+                    Save
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal >
+      </>
+    );
+  }
+
+
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-        <br />
-        <h1 className={title()}>
-          websites regardless of your design experience.
-        </h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </h2>
-      </div>
+    <section>
+      <Button onClick={settingsModal.onOpenChange}>This works</Button>
 
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
 
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="flat">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
+      <Dropdown className="dark text-foreground">
+        <DropdownTrigger>
+          <Button className="self-center hover:bg-neutral-600">
+            This doesnt work
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu
+          aria-label='actions-dropdown-menu'
+          onAction={(key: any) => {
+            if (key === 'settings') {
+              settingsModal.onOpenChange();
+            }
+          }}
+
+        >
+          <DropdownItem key='settings'>
+            click me
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+
+
+
+      <SettingsModal disclosure={settingsModal} />
     </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-geist-mono)"],
+       sans: ['Helvetica', 'Arial', 'sans-serif'],
       },
     },
   },
